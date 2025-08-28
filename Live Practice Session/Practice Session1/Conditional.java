14d79bb724d227673aa757382c51a26568a3d4f5
dillipacharya19 <dillipacharya12345@gmail.com>
2025-08-28 14:36:53 +0530

    i added Q3

--- Live Practice Session/Practice Session1/Conditional.java
+++ Live Practice Session/Practice Session1/Conditional.java
@@ -55,36 +55,51 @@
 // Q-3 Write a java program to input week number (1-7) and print day of week
 // name using switch case.
 
-import java.util.*;
-public class Conditional {
+// import java.util.*;
+// public class Conditional {
+//     public static void main(String[] args) {
+//         Scanner sc= new Scanner(System.in);
+//         int day = sc.nextInt();
+//         switch (day) {
+//             case 1
+//                 System.out.println("sunday");
+//                 break;
+//             case 2:
+//                 System.out.println("monday");
+//             break;
+//             case 3:
+//                 System.out.println("tuesday");
+//             break;
+//             case 4:
+//                 System.out.println("wednesday");
+//             break;
+//             case 5:
+//                 System.out.println("thursday");
+//             break;
+//             case 6:
+//                 System.out.println("friday");
+//             break;
+//             case 7:
+//             System.out.println("saturday");
+//             break;
+//             default:
+//                 System.out.println("Invaild input");
+//         }
+//     }
+// }
 
+
+
+
+
+
+
+// Q-4 What will be the value of x&y in the following program.
+
+public class Conditional {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int day = sc.nextInt();
-        switch (day) {
-            case 1
-                System.out.println("sunday");
-                break;
-            case 2:
-                System.out.println("monday");
-            break;
-            case 3:
-                System.out.println("tuesday");
-            break;
-            case 4:
-                System.out.println("wednesday");
-            break;
-            case 5:
-                System.out.println("thursday");
-            break;
-            case 6:
-                System.out.println("friday");
-            break;
-            case 7:
-            System.out.println("saturday");
-            break;
-            default:
-                System.out.println("Invaild input");
-        }
+        int a = 63 , int b = 36;
+        boolean x = (a<b) ? true : false;
+        int y = (a>b) ? a : b;
     }
 }
