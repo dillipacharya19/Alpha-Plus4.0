7c5848ff9bbd98bf9cfb1443435b83c90acb17f4
dillipacharya19 <dillipacharya12345@gmail.com>
2025-08-28 15:00:53 +0530

    i added Q1

--- Live Practice Session/Practice Session-5/loops.java
+++ Live Practice Session/Practice Session-5/loops.java
@@ -1,5 +1,11 @@
-package Live Practice Session.Practice Session-5;
+// Q-1 How many times Hello is printed ? 
 
 public class loops {
-    
+
+    public static void main(String[] args) {
+        for(int i =0; i<5; i++){
+            System.out.println("hello");
+            i+=2;
+        }
+    }
 }
