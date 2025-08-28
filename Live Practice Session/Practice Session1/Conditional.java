353596a53f6f3ee07014da67a3de17820dd44f1f
dillipacharya19 <dillipacharya12345@gmail.com>
2025-08-28 12:23:15 +0530

    first commit

---
+++ Live Practice Session/Practice Session1/Conditional.java
@@ -0,0 +1,19 @@
+// 1- if else 
+// 2- else if 
+// 3- Ternary Operator
+// 4- Switch Statement
+
+
+public class Conditional {
+    public static void main(String []args){
+        int a = 3;
+        int b = 4;
+
+        if (a == b) {
+            System.out.println("a is equal b");
+        } else {
+            System.out.println("a is not equal b");
+        }
+    }
+    
+}
