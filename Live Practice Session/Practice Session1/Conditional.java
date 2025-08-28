89f1ae2f704f936bc635926582ec4d994d1b685a
dillipacharya19 <dillipacharya12345@gmail.com>
2025-08-28 12:43:53 +0530

    i added Q2

--- Live Practice Session/Practice Session1/Conditional.java
+++ Live Practice Session/Practice Session1/Conditional.java
@@ -1,23 +1,43 @@
-// 1- if else 
-// 2- else if 
+// 1- if else
+// 2- else if
 // 3- Ternary Operator
 // 4- Switch Statement
 
+// Q-1 Write a java program to get a number from the user and print whether it
+// is positive or negative
 
-// Q-1 Write a java program to get a number from the user and print whether it is positive or negative
+// import java.util.*;
+// public class Conditional {
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int num = sc.nextInt();
+
+// if (num >= 0) {
+// System.out.println(num+ " Positive Number");
+// }
+// else{
+// System.out.println(num+ " Negative Number");
+// }
+// }
+// }
+
+// Q-2 Finish the following code so that it prints you have a fever if your
+// tempreture is above 100 and otherwise prints you don't have fever.
 
 import java.util.*;
+
 public class Conditional {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
 
-        if (num >= 0) {
-            System.out.println(num+ " Positive Number");
-        }
-        else{
-            System.out.println(num+ " Negative Number");
+        int temp = sc.nextInt();
+
+        if (temp >= 100) {
+            System.out.println("You have Fever");
+        } else {
+            System.out.println("You don't have Fever");
         }
     }
 }
