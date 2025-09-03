e9dc6b68194f6c25a2c14992ebdf16a5c78b5c08
dillipacharya19 <dillipacharya12345@gmail.com>
2025-09-03 23:32:30 +0530

    i added for loop

---
+++ Live Class/11Loops/for_loop/for-loop/for_loop.java
@@ -0,0 +1,10 @@
+
+
+public class for_loop {
+    public static void main(String[] args) {
+        // int i = 1;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello world");
+        }
+    }
+}
