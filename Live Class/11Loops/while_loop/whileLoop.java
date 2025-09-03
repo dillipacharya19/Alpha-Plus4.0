f506c44e532549cbdb0c4145fa1315c65c974aa1
dillipacharya19 <dillipacharya12345@gmail.com>
2025-09-03 22:21:38 +0530

    i added while loop

---
+++ Live Class/11Loops/while_loop/whileLoop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 100) {
+            System.out.println("Hello world");
+            counter++;
+        }
+        System.out.println("Printed hello world 100x");
+    }
+}
