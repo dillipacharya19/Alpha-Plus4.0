3c72716f2525cdf025c413a201587cb9648c2cad
dillipacharya19 <dillipacharya12345@gmail.com>
2025-09-06 19:20:03 +0530

    I added loop break statement

---
+++ Live Class/11Loops/breakStatement/hello.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class hello {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("I am Out Of The Loop");
+    }
+}
