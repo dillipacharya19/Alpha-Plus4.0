1d236d28428175ce533e4cea9170f7e9f220db4a
dillipacharya19 <dillipacharya12345@gmail.com>
2025-09-08 09:16:28 +0530

    i solved problem 4

---
+++ Live Practice Session/SundayLiveSession-1/Problem-4/hello.java
@@ -0,0 +1,13 @@
+public class hello {
+public static void main(String[] args) {
+    for(int i = 5; i >= 1; i--) {
+        for(int x = 1; x <i; x++){
+            System.out.print(" ");
+        }
+        for(int j = 5; j>= i; j--){
+            System.out.print(j);
+        }
+        System.out.println("");
+        }
+    }
+}
