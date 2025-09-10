455f0a48425a58730ede6f3a1fba10cd4876d388
dillipacharya19 <dillipacharya12345@gmail.com>
2025-09-10 09:06:31 +0530

    i added function demo

---
+++ Live Class/13-Functions-and-Methods/Syntax-Function-Code-Demo/demo.java
@@ -0,0 +1,11 @@
+public class demo {
+    public static void printHelloWorld() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        printHelloWorld();
+        printHelloWorld();
+        printHelloWorld();
+    }
+}
