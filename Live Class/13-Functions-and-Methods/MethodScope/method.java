c261f21fd5058189e47dfc74add96cabb63c7f5c
dillipacharya19 <dillipacharya12345@gmail.com>
2025-09-12 20:28:29 +0530

    i added method

---
+++ Live Class/13-Functions-and-Methods/MethodScope/method.java
@@ -0,0 +1,11 @@
+public class method {
+   public static void prints(){
+    int s =45;
+
+   }
+   public static void main(String[] args) {
+    System.out.println(s);
+   }
+}
+
+// We cant access the variable s in main method because it is defined in prints method and its scope is limited to prints method only.
