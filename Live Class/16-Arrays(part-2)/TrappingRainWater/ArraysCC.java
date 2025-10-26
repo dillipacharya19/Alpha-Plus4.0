6e6760775cccde4a3d152638efb349866370b353
dillipacharya19 <dillipacharya12345@gmail.com>
2025-10-26 22:19:44 +0530

    i added trapping rain water

---
+++ Live Class/16-Arrays(part-2)/TrappingRainWater/ArraysCC.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class ArraysCC {
+
+    public static int trappingRainWater(int height[]) {
+        int n = height.length;
+        //calculte left max boundary - array
+        int leftMax[] = new int[n];
+        int rightMax[] = new int[n];
+
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
+        //calculate right max boundary - array
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+
+        int trappedWater = 0;
+        //loop
+        for (int i = 0; i < n; i++) {
+            //water level = min(leftmax, rightmax)
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            //trapped water = waterlevel - height[i]
+            trappedWater += waterLevel - height[i];
+        }
+
+        return trappedWater;
+    }
+    public static void main(String args[]) {
+        int height[] = {4, 2, 0, 6, 3, 2, 5};
+        System.out.println("The total trapped rain water is: " + trappingRainWater(height));
+    }
+}
