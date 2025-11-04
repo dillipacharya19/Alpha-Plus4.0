271561005af8c6d31c6a6484766ffc0b0c1e9b9f
dillipacharya19 <dillipacharya12345@gmail.com>
2025-11-04 10:05:42 +0530

    i added bubble sort

---
+++ Live Class/17-BasicSortingAlgorithms/BubbleSort/Bubble.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Bubble {
+    public static void bubbleSort(int arr[]){
+        for( int turn = 0; turn < arr.length -1; turn++){
+            for( int j = 0; j < arr.length -1 - turn; j++){
+                if( arr[j] > arr[j+1]){
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void PrintArr(int arr[]){
+        for(int i =0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.err.println();
+    }
+    public static void main(String args[]){
+        int arr[] = {5,4,1,3,2};
+        bubbleSort(arr);
+       PrintArr(arr);
+    }
+}
