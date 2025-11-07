dd99850132ea5acc750b924ef22c892d7d0c4227
dillipacharya19 <dillipacharya12345@gmail.com>
2025-11-07 11:05:13 +0530

    i added What are Strings

---
+++ Live Class/19-Strings/What are Strings/Strings.java
@@ -0,0 +1,9 @@
+public class Strings{
+    public static void main(String[] args) {
+        char[] chars = {'H', 'e', 'l', 'l', 'o'};
+        String str = "Hello";
+        String str2 = new String("xyz@#123");
+        //Strings are immutable
+        System.out.println(chars);
+    }
+}
