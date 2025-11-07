370ad9ab12dcf55c9683d13d7b04314b07a654a3
dillipacharya19 <dillipacharya12345@gmail.com>
2025-11-07 11:14:10 +0530

    i added java input or output

---
+++ Live Class/19-Strings/Input or Output/index.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class index{
+    public static void main(String[] args) {
+        char arr []= {'H','e','l','l','o',',',' ','W','o','r','l','d','!'};
+        String str = "abcdefg";
+        String str2 = new String("xyz@#123");
+        //Strings are immutable in Java
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
