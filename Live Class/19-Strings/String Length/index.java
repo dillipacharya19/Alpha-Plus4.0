70bb31251cb86165edf06972f8ebb3eca63a0dc3
dillipacharya19 <dillipacharya12345@gmail.com>
2025-11-08 21:21:05 +0530

    i added String length

---
+++ Live Class/19-Strings/String Length/index.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class index{
+    public static void main(String[] args) {
+        String fullName="Tony Stark";
+        System.out.println(fullName.length());
+    }
+}
