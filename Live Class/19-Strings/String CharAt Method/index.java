1949d31efa4fbd68adfa047d31b7f95bd7d2e408
dillipacharya19 <dillipacharya12345@gmail.com>
2025-11-08 21:54:33 +0530

    i added String CharAt method

---
+++ Live Class/19-Strings/String CharAt Method/index.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class index {
+    public static void main(String[] args) {
+        String firstName = "Dillip";
+        String lastName = "Acharya";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName.charAt(0));
+
+    }
+}
