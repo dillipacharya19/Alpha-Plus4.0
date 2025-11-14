29bf4bc8fb51c7cb4dcf22520f269c4d681296f0
dillipacharya19 <dillipacharya12345@gmail.com>
2025-11-14 22:51:14 +0530

    i added string function compare

---
+++ Live Class/19-Strings/String Functions Compare/index.java
@@ -0,0 +1,18 @@
+
+public class index{
+    public static void main(String[] args){
+        String s1 ="Tony";
+        String s2 = "Tony";
+        String s3 = new String("Tony");
+        // if(s1==s3){
+        //     System.out.println("strings are equal ");
+        // }else{
+        //     System.out.println("strings are not equal ");
+        //     }
+            if(s1.equals(s3)){
+                System.out.println("strings are equal using equals method");
+        }else{
+            System.out.println("strings are not equal using equals method");
+        }
+    }
+}
