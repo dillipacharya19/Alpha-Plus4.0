632103a6f46e12b4fe7a43adf8cd0e75c626365a
dillipacharya19 <dillipacharya12345@gmail.com>
2025-11-14 21:45:50 +0530

    i added check if a number is a palindrome

---
+++ Live Class/19-Strings/Check If a Number is a Palindrome/index.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class index {
+    public static boolean isPalindrome(String str) {
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            int n = str.length();
+            if (str.charAt(i) != str.charAt(n - i - 1)) {
+                // Not a Palindrome
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(isPalindrome(str));
+    }
+}
