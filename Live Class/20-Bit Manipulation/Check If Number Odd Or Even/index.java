2f02ea58318f1c2c6a559fc01751d12791958022
dillipacharya19 <dillipacharya12345@gmail.com>
2025-11-18 08:38:09 +0530

    i added if check a number is odd or even

---
+++ Live Class/20-Bit Manipulation/Check If Number Odd Or Even/index.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class index {
+    public static void oddOrEven(int n) {
+        int bitMask = 1;
+        if ((n & bitMask) == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+    }
+
+    public static void main(String[] args) {
+        oddOrEven(3);
+        oddOrEven(11);
+        oddOrEven(14);
+    }
+}
