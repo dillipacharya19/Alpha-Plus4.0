1cd26213969ddd4ca308d359ded549f888fc57e9
dillipacharya19 <dillipacharya12345@gmail.com>
2025-11-19 20:49:26 +0530

    i added clear last i bits

--- Live Class/20-Bit Manipulation/Clear Last i Bits/index.java
+++ Live Class/20-Bit Manipulation/Clear Last i Bits/index.java
@@ -19,6 +19,7 @@ public class index {
     public static int clearIthBits(int n, int i) {
         int bitMask = (~0) << i;
         return n & bitMask;
+
     }
 
     public static void main(String[] args) {
