cc8986e9cf94a552f6ee88d6bede05476d3426b2
dillipacharya19 <dillipacharya12345@gmail.com>
2025-11-19 22:02:54 +0530

    i added check if a number is power of 2 or not

---
+++ Live Class/20-Bit Manipulation/Check If a Number is Power of 2 Or Not/index.java
@@ -0,0 +1,11 @@
+public class index {
+    public static boolean isPowerOfTwo(int n) {
+
+        return (n & (n - 1)) == 0;
+
+    }
+
+    public static void main(String args[]) {
+        System.out.println(isPowerOfTwo(15));
+    }
+}
