873eded7a3bfa4e68e468b855c1e9405e058d400
dillipacharya19 <dillipacharya12345@gmail.com>
2025-11-19 20:22:54 +0530

    i added update ith bit

---
+++ Live Class/20-Bit Manipulation/Update Ith Bit/index.java
@@ -0,0 +1,22 @@
+public class index {
+    public static int clearIthBit(int n, int i) {
+        int bitMask = ~(1 << i);
+        return n & bitMask;
+    }
+
+    public static int updateIthBit(int n, int i, int newBit) {
+        // if(newBit == 0){
+        // return clearIthBit(n, i);
+        // }else{
+        // return n | (1 << i);
+        // }
+        n = clearIthBit(n, i);
+        int bitMask = newBit << i;
+        return n | bitMask;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(updateIthBit(10, 2, 1));
+    }
+}
