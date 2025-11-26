df7d5c5b5337e2637d9aca9cd668e9775fef5c49
dillipacharya19 <dillipacharya12345@gmail.com>
2025-11-26 22:32:07 +0530

    i added getter and setter using this keyword

---
+++ Live Class/21-Object Oriented Programing(OOPS)/Getters And Setters/OOPS.java
@@ -0,0 +1,33 @@
+public class OOPS {
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        System.out.println(p1.getColor());
+        p1.SetTip(5);
+        System.out.println(p1.getTip());
+        p1.setColor("Yellow");
+        System.out.println(p1.getColor());
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    void SetTip(int newTip) {
+        this.tip = newTip;
+    }
+
+}
