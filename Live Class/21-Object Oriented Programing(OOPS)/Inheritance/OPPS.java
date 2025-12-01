6d20ccbf21ee4277b011c910fb9e2507a0032f4f
dillipacharya19 <dillipacharya12345@gmail.com>
2025-12-01 23:20:14 +0530

    i added inheritance

---
+++ Live Class/21-Object Oriented Programing(OOPS)/Inheritance/OPPS.java
@@ -0,0 +1,28 @@
+public class OPPS {
+    public static void main(String[] args) {
+        Fish shark = new Fish();
+        shark.eat();
+    }
+}
+// Base class
+
+class Animal {
+    String color;
+
+    void eat() {
+        System.out.println("eats");
+    }
+
+    void breath() {
+        System.out.println("breathes");
+    }
+}
+
+// Derived class
+class Fish extends Animal {
+    int fins;
+
+    void swim() {
+        System.out.println("swims in a water");
+    }
+}
