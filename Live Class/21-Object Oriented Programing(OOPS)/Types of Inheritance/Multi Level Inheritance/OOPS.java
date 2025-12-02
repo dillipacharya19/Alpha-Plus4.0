c3cd8524059fd804616c3afd4e7a9023cde690d8
dillipacharya19 <dillipacharya12345@gmail.com>
2025-12-02 23:13:33 +0530

    i added oops multi level inheritance

---
+++ Live Class/21-Object Oriented Programing(OOPS)/Types of Inheritance/Multi Level Inheritance/OOPS.java
@@ -0,0 +1,31 @@
+public class OOPS {
+    public static void main(String[] args) {
+        Dog dobby = new Dog();
+        dobby.eat();
+        dobby.legs = 4;
+        System.out.println(dobby.legs);
+    }
+}
+
+// Base class
+class Animal {
+    String color;
+
+    void eat() {
+        System.out.println("eating...");
+    }
+
+    // Derived class
+    void breath() {
+        System.out.println("breathing...");
+    }
+}
+
+class Mammal extends Animal {
+    int legs;
+}
+
+class Dog extends Mammal {
+
+    String breed;
+}
