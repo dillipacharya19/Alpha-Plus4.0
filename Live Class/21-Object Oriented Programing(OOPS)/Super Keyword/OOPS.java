40afd54dea5f9d351616ac9cc0e83438d8c7812e
dillipacharya19 <dillipacharya12345@gmail.com>
2025-12-04 08:54:55 +0530

    i added super keyword in java

---
+++ Live Class/21-Object Oriented Programing(OOPS)/Super Keyword/OOPS.java
@@ -0,0 +1,22 @@
+public class OOPS {
+    public static void main(String[] args) {
+        Horse h = new Horse();
+        System.out.println(h.color);
+    }
+}
+
+class Animal {
+    String color;
+
+    Animal() {
+        System.out.println("Animal constructer is called");
+    }
+}
+
+class Horse extends Animal {
+    Horse() {
+        super();
+        color = "Brown";
+        System.out.println("Horse constructer is called");
+    }
+}
