2142f08f4da0e3d2c362c07497e69d43a02bd4db
dillipacharya19 <dillipacharya12345@gmail.com>
2025-12-05 20:22:38 +0530

    i added question-4

---
+++ Live Class/21-Object Oriented Programing(OOPS)/Practice Question/Question-4/index.java
@@ -0,0 +1,10 @@
+// // Which of the following is a correct statement ? (both classes in same package)
+
+// class Vehical {
+// }
+
+// class car extends Vehical{}
+// a- car c = new car(); 
+// b- Vehical v= new Vehical();
+// c- Vehical v = new car();
+// d- car c = new Vehical();
