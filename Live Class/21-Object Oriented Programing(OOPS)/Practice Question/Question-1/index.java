e8a37d83484a2f72ae900fb0e7a1aa588374d39f
dillipacharya19 <dillipacharya12345@gmail.com>
2025-12-05 19:53:55 +0530

    i added Find the correct statement to assign name to object practice question-1

---
+++ Live Class/21-Object Oriented Programing(OOPS)/Practice Question/Question-1/index.java
@@ -0,0 +1,13 @@
+//Find the correct statement to assign name to object
+class student {
+    String name;
+    int marks;
+
+}
+
+public class index {
+    public static void main(String[] args) {
+        student c = new student();
+        //File Here
+    }
+}
