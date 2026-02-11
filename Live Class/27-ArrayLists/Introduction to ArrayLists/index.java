00155f69753d40b95e20861ff8917b3c128e1fb0
dillipacharya19 <dillipacharya12345@gmail.com>
2026-02-11 21:32:01 +0530

    i added introduction to arrayLists

---
+++ Live Class/27-ArrayLists/Introduction to ArrayLists/index.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class index {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Boolean> list2 = new ArrayList<>();
+
+    }
+}
