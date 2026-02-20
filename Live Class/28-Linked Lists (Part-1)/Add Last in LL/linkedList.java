a62dd9652444c87e5ab676d9739402506cb2e0e4
dillipacharya19 <dillipacharya12345@gmail.com>
2026-02-20 20:34:41 +0530

    i added add last in LL

---
+++ Live Class/28-Linked Lists (Part-1)/Add Last in LL/linkedList.java
@@ -0,0 +1,33 @@
+public class linkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public static void main(String[] args) {
+        linkedList ll = new linkedList();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+    }
+}
