705b02087eed77bcc819b2c630dfc09c0ebbacb6
dillipacharya19 <dillipacharya12345@gmail.com>
2026-02-20 23:01:28 +0530

    i added add in the middle of ll

---
+++ Live Class/28-Linked Lists (Part-1)/Add in the middle of LL/linkedList.java
@@ -0,0 +1,85 @@
+public class linkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        // step 1: create new node
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        // step 2: newNode next = head
+        newNode.next = head;
+        // step 3: head = newNode
+        head = newNode;
+
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("Linked List is empty");
+            return;
+
+        }
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void add(int idx, int data) {
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+
+        // i = idx-1; temp -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+    }
+
+    public static void main(String[] args) {
+        linkedList ll = new linkedList();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.add(2, 9);
+        ll.print();
+
+    }
+}
