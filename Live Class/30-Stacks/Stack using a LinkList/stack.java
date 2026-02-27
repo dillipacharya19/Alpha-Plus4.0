89ab6773d96f3133fb7199e9e2534bea48e0a2c2
dillipacharya19 <dillipacharya12345@gmail.com>
2026-02-27 10:06:35 +0530

    i added stack using a linkList

---
+++ Live Class/30-Stacks/Stack using a LinkList/stack.java
@@ -0,0 +1,54 @@
+public class stack {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class CustomStack {
+        static Node head = null;
+
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        public static void push(int data) {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    // ✅ Main method in the outer class
+    public static void main(String[] args) {
+        CustomStack s = new CustomStack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+}
