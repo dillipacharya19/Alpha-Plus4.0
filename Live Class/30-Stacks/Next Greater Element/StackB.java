eceb4f048a4157900fd04014e1ee14bd67cdc98f
dillipacharya19 <dillipacharya12345@gmail.com>
2026-03-01 10:54:07 +0530

    i added next greater element

---
+++ Live Class/30-Stacks/Next Greater Element/StackB.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class StackB {
+    public static void main(String[] args) { // 0(n)
+        int arr[] = { 6, 8, 0, 1, 3 };
+        Stack<Integer> s = new Stack<>();
+        int nxtGreater[] = new int[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            // while
+            while (!s.isEmpty() && arr[s.peek()] <= arr[i]) {
+                s.pop();
+            }
+            // 2 if else
+            if (s.isEmpty()) {
+                nxtGreater[i] = -1;
+            } else {
+                nxtGreater[i] = arr[s.peek()];
+            }
+
+            // 3 push in stack
+            s.push(i);
+        }
+        for (int i = 0; i < nxtGreater.length; i++) {
+            System.out.print(nxtGreater[i] + " ");
+        }
+        System.out.println();
+
+        // Next greater to right
+        // Next greater to left
+        // Next smaller to right
+        // Next smaller to left
+    }
+}
