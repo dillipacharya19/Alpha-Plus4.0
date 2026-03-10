268d654179f6bd95cdc9b22502166ff2152a9b88
dillipacharya19 <dillipacharya12345@gmail.com>
2026-03-10 21:01:36 +0530

    i added build tree preorder

---
+++ Live Class/33-Binary Trees (Part-1)/Build Tree Preorder/Classroom.java
@@ -0,0 +1,36 @@
+
+public class Classroom {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static int idx = -1;
+
+    public static Node buildTree(int nodes[]) {
+        idx++;
+        if (nodes[idx] == -1) {
+            return null;
+        }
+
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+        return newNode;
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 3, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+
+        idx = -1; // reset before building
+        Node root = buildTree(nodes);
+        System.out.println(root.data); // should print 1 (root node)
+    }
+}
