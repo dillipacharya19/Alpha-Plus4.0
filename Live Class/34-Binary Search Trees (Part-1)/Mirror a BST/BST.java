d79c40dba3e1dffee6378b64c9d1959a818118d8
dillipacharya19 <dillipacharya12345@gmail.com>
2026-03-19 14:25:52 +0530

    i added mirror BST

---
+++ Live Class/34-Binary Search Trees (Part-1)/Mirror a BST/BST.java
@@ -0,0 +1,62 @@
+public class BST {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+
+    // Create mirror of BST
+    public static Node createMirror(Node root) {
+        if (root == null) return null;
+
+        Node leftMirror = createMirror(root.left);
+        Node rightMirror = createMirror(root.right);
+
+        root.left = rightMirror;
+        root.right = leftMirror;
+
+        return root;
+    }
+
+    // Preorder traversal
+    public static void preorder(Node root) {
+        if (root == null) return;
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        // Build tree manually
+        Node root = new Node(8);
+        root.left = new Node(5);
+        root.right = new Node(10);
+        root.left.left = new Node(3);
+        root.left.right = new Node(6);
+        root.right.right = new Node(11);
+
+        /*
+            Original BST:
+                  8
+                 / \
+                5   10
+               / \    \
+              3   6    11
+
+            Mirror BST:
+                  8
+                 / \
+               10   5
+              /    / \
+            11    6   3
+        */
+
+        root = createMirror(root);
+        System.out.println("Preorder of mirrored tree:");
+        preorder(root);
+    }
+}
